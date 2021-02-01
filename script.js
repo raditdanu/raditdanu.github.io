3099f780c1cefa6070153e73ed6c6851188f7c1e
Radit Danu <radityadanue@gmail.com>
2021-02-01 23:45:52 +0700

    update 1

--- script.js
+++ script.js
@@ -44,24 +44,25 @@ $(document).ready(function() {
    
 
     // $('.carousel').owlCarousel({
-    //     margin: 20,
-    //     loop: true,
-    //     autoplayTimeOut: 2000,
-    //     autoplayHoverPause: true,
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             nav: false
-    //         },
-    //         600: {
-    //             items: 2,
-    //             nav: false
-    //         },
-    //         1000: {
-    //             items: 3,
-    //             nav: false
-    //         }
-    //     }
+    //     margin: 20
+        
+    //     // loop: true,
+    //     // autoplayTimeOut: 2000,
+    //     // autoplayHoverPause: true,
+    //     // responsive: {
+    //     //     0: {
+    //     //         items: 1,
+    //     //         nav: false
+    //     //     },
+    //     //     600: {
+    //     //         items: 2,
+    //     //         nav: false
+    //     //     },
+    //     //     1000: {
+    //     //         items: 3,
+    //     //         nav: false
+    //     //     }
+    //     // }
     // });
 });
 
